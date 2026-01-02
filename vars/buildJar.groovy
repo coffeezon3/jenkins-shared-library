d48030bacb03ce8febb420a22ad407695150eaf4
coffeezon3 <227237350+coffeezon3@users.noreply.github.com>
2026-01-02 18:52:24 +0100

    Update buildJar.groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,7 @@
 #!/user/bin/env groovy
 
-def call() {
-    echo "building the application for branch $GIT_BRANCH"
-    sh 'mvn package'
+import com.example.Docker
+
+def call(String imageName) {
+    return new Docker(this).buildDockerImage(imageName)
 }
