232e2eed8f45e1373f5de7c2ad845af85ec48d33
coffeezon3 <227237350+coffeezon3@users.noreply.github.com>
2026-01-02 23:05:14 +0100

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,11 +1,4 @@
-def call() {
-    withCredentials([usernamePassword(
-        credentialsId: 'dockerhub-creds',
-        usernameVariable: 'USER',
-        passwordVariable: 'PASS'
-    )]) {
-        sh '''
-          echo "$PASS" | docker login -u "$USER" --password-stdin
-        '''
-    }
+def call(String imageName) {
+    sh "docker push ${imageName}"
 }
+
